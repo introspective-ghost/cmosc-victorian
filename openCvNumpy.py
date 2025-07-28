1cfaf1f03346328de8a791d8d7a2c1d33c2d1653
Josh Weadick <joshccie2415@gmail.com>
2025-07-28 07:44:54 +0100

    create fullscreen window

--- openCvNumpy.py
+++ openCvNumpy.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from picamera2 import Picamera2, Preview
 import time
+import tkinter as tk
 
 
 def exitCleanup():
@@ -9,11 +10,18 @@ def exitCleanup():
     cv2.destroyAllWindows()
     exit()
 
+root = tk.Tk()
+screenWidth = root.winfo_screenwidth()
+screenHeight = root.winfo_screenheight()
+root.destroy()
+
+print("Screen Width: " + str(screenWidth))
+print("Screen Height: " + str(screenHeight))
 picam2 = Picamera2()
 picam2.options["quality"] = 90
 picam2.options["compress_level"] = 2
 config = picam2.create_preview_configuration(main={"size": (800, 600)})
-picam2.start_preview(Preview.NULL, x=100, y=200, width=1280, height=720)
+picam2.start_preview(Preview.NULL)
 picam2.configure(config)
 picam2.start()
 time.sleep(2)
@@ -31,13 +39,21 @@ hb, wb = bg.shape[:2]
 # Define green range and make mask
 lower = np.array([36, 50, 50])
 upper = np.array([90, 255, 255])
+
+# Create fullscreen window
+cv2.namedWindow("Greenscreen Composite", cv2.WINDOW_NORMAL)
+cv2.setWindowProperty("Greenscreen Composite", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
 while True:
     frame_rgba = picam2.capture_array("main")  # BGR
+    # Remove the alpha channel from the image
     frame = frame_rgba[..., :3]
+    # Grab the height and width from the frame
     h, w = frame.shape[:2]
     
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower, upper)
+    # Make sure the frame mask and frame background will be the same size
     if mask.shape != (h, w):
         mask = cv2.resize(mask, (w, h), interpolation=cv2.INTER_NEAREST)
     if bg.shape != (h, w):
@@ -47,7 +63,6 @@ while True:
     fg = cv2.bitwise_and(frame, frame, mask=mask_inv)
     bk = cv2.bitwise_and(bg, bg, mask=mask)
     output = cv2.add(fg, bk)
-
     cv2.imshow("Greenscreen Composite", output)
     # Press the escape key with the Greenscreen Composite window in focus to end the program
     if cv2.waitKey(1) == 27:
